356bca10b84a6a6b9c164e7d16b5f5514464f06d
xiangshaoxiong <xiangshaoxiong@wavewisdom.com>
2022-03-11 17:42:40 +0800

    [解决ActivityThread启动报错]

--- com/wave/ActivityThread.java
+++ com/wave/ActivityThread.java
@@ -2,6 +2,7 @@
 package com.wave;
 
 import com.wave.ContextImpl;
+import com.wave.Handler;
 public final class ActivityThread  {
 
 
@@ -11,12 +12,16 @@ public final class ActivityThread  {
         // The system process on low-memory devices do not get to use hardware
         // accelerated drawing, since this can add too much overhead to the
         // process.
+        System.out.println("--systemMain--");
         ActivityThread thread = new ActivityThread();
 //         thread.attach(true, 0);
         return thread;
+//         return null;
     }
 
     ActivityThread() {
+        System.out.println("--ActivityThread--");
+
 //             mResourcesManager = ResourcesManager.getInstance();
     }
 
@@ -29,5 +34,50 @@ public final class ActivityThread  {
             return mSystemContext;
         }
     }
+
+    static volatile Handler sMainThreadHandler;  // set once in main()
+    final H mH = new H();
+
+    final Handler getHandler() {
+            System.out.println("getHandler");
+            return mH;
+    }
+    public static void main(String[] args) {
+        System.out.println("--main--");
+        Looper.prepareMainLooper();
+
+        ActivityThread thread = new ActivityThread();
+//         thread.attach(false, 0);
+
+        if (sMainThreadHandler == null) {
+                sMainThreadHandler = thread.getHandler();
+        }
+        Looper.loop();
+    }
+
+    class H extends Handler {
+
+            public void handleMessage(Message msg) {
+                System.out.println("msg "+msg.what);
+            }
+    }
+//     private void attach(boolean system, long startSeq) {
+//             final IActivityManager mgr = ActivityManager.getService();
+//             try {
+//                 mgr.attachApplication(mAppThread, startSeq);
+//             } catch (RemoteException ex) {
+//                 throw ex.rethrowFromSystemServer();
+//             }
+//     }
+//     public Activity handleLaunchActivity(ActivityClientRecord r,
+//             PendingTransactionActions pendingActions, Intent customIntent) {
+//         System.out.println("handleLaunchActivity " );
+//
+//     }
+
+//     public void handleResumeActivity(IBinder token, boolean finalStateRequest, boolean isForward,String reason) {
+//         System.out.println("handleResumeActivity " );
+//
+//     }
 
 }
--- com/wave/SystemServer.java
+++ com/wave/SystemServer.java
@@ -19,6 +19,8 @@ public final class SystemServer {
             new SystemServer().run();
     }
     private void run() {
+
+        Looper.prepareMainLooper();
         mSystemServiceManager = new SystemServiceManager();
 
         createSystemContext();
@@ -34,6 +36,8 @@ public final class SystemServer {
         } finally {
             System.out.println("finally ");
         }
+        Looper.loop();
+
     }
 
     private void createSystemContext() {
