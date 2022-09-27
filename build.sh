9d81148a5a7f79098c00bd0a2d8f611368983697
xiangshaoxiong <xiangshaoxiong@wavewisdom.com>
2022-09-27 14:42:50 +0800

    [MASTER][修改编译脚本]

--- build.sh
+++ build.sh
@@ -24,10 +24,12 @@ cd ${VPATH}
 echo $(pwd)
 echo "编译app_process"
 javac  com/wave/ZygoteInit.java
+javac  com/wave/SystemServer.java
+gcc -D__int64="long long" -I/usr/lib/jvm/java-8-openjdk-amd64/include  -I/usr/lib/jvm/java-8-openjdk-amd64/include/linux -o app_process app_process.c  -L/usr/lib/jvm/java-8-openjdk-amd64/jre/lib/amd64/server -ljvm
+
+echo "编译package app"
 javac  com/packages/Launcher.java
 javac  com/packages/Settings.java
 javac  com/packages/Picture.java
-javac  com/wave/SystemServer.java
-gcc -D__int64="long long" -I/usr/lib/jvm/java-8-openjdk-amd64/include  -I/usr/lib/jvm/java-8-openjdk-amd64/include/linux -o app_process app_process.c  -L/usr/lib/jvm/java-8-openjdk-amd64/jre/lib/amd64/server -ljvm
 echo "执行.."
 ./app_process
